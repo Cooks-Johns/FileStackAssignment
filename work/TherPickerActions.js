55b6ee730468f4ea2a142424df32a127eafa835a
John <cooks.johns@gmail.com>
2018-07-23 18:26:36 -0500

    open button fixing

--- work/TherPickerActions.js
+++ work/TherPickerActions.js
@@ -4,19 +4,20 @@
 
 window.addEventListener('DOMContentLoaded', function () {
     const apikey = 'ASkdROusXR2KK14hNk11Kz';
-
-
     const client = filestack.init(apikey);
-
-
-
-
     const options = {
         maxFiles: 5,
         uploadInBackground: false,
-
+        display: inline,
+        onUploadDon: (res) => console.log(res)
 
     };
+
+
+
+
+
+
     const picker = ()=> {
 
         client.pick({maxFiles: 5, })
