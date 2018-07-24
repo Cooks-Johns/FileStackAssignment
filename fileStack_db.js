93190af354ec7fdae6968518b8a17165caacf12c
John <cooks.johns@gmail.com>
2018-07-24 11:14:52 -0500

    fixed open picker Button

---
+++ fileStack_db.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// export function fileStack_db(sqlQuery) {
+//     let con = mysql.createConnection({
+//         host: localhost,
+//         user: filestack,
+//         password, password
+//     })
+// }
--- work/TherPickerActions.js
+++ work/TherPickerActions.js
@@ -5,12 +5,14 @@
 window.addEventListener('DOMContentLoaded', function () {
     const apikey = 'ASkdROusXR2KK14hNk11Kz';
     const client = filestack.init(apikey);
+    const container = document.getElementById('displayPics');
+
     const options = {
         displayMode: 'inlineDisplay',
         container: '#inlineDisplay',
         maxFiles: 5,
         uploadInBackground: false,
-        onUploadDone: (res) => console.log(res),
+        onUploadDone: (res) => console.log(res)
     };
 
 
@@ -19,8 +21,11 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const picker = client.picker(options);
     const openBtn = document.getElementById('openPicker');
-    const closeBtn = document.getElementById('close');
+
     openBtn.addEventListener('click', () => picker.open());
-    closeBtn.addEventListener('click', () => picker.close());
+
+
+
 
 });
+
